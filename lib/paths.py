56482ad81a12e0fb2c92d6e5a33af958f55793e4
Pr0Ger <git@pr0ger.org>
2012-07-24 16:58:44 +0400

    Added detecting Steam paths

--- lib/paths.py
+++ lib/paths.py
@@ -1,6 +1,9 @@
+import os
 import platform
 
+
 MyDocumentsPath = None
+SteamGamesPath = None
 
 if platform.system() == 'Windows':
     import ctypes
@@ -10,3 +13,12 @@ if platform.system() == 'Windows':
     buf = ctypes.create_unicode_buffer(MAX_PATH + 1)
     if dll.SHGetSpecialFolderPathW(None, buf, 0x0005, False):
         MyDocumentsPath = buf.value
+
+    from winreg import HKEY_CURRENT_USER, OpenKey, QueryValueEx
+
+    try:
+        key = OpenKey(HKEY_CURRENT_USER, 'Software\\Valve\\Steam')
+        SteamPath = QueryValueEx(key, 'SteamPath')[0]
+        SteamGamesPath = os.path.join(SteamPath, 'steamapps', 'common')
+    except WindowsError:
+        pass
