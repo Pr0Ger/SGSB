3a0e0f25e8f24db221c5d3fa5e4417c5c48eb869
Pr0Ger <git@pr0ger.org>
2012-09-13 23:56:25 +0400

    All S.T.A.L.K.E.R. plugins was consolidated to one file

--- plugins/STALKER.py
+++ plugins/STALKER.py
@@ -3,23 +3,31 @@ from lib.base_plugin import BasePlugin
 from lib.paths import SteamGamesPath
 
 
-class STALKERPlugin(BasePlugin):
-    Name = "S.T.A.L.K.E.R.: Shadow of Chernobyl (RU)"
-    support_os = ["Windows"]
+def factory(name, folder):
+    class TempPlugin(BasePlugin):
+        Name = name
+        support_os = ["Windows"]
 
-    def backup(self):
-        super().backup()
-        path_to_appdata = os.path.join(SteamGamesPath, 'stalker shadow of chernobyl ru', '_appdata_')
-        self.backup_file.add_folder('Saves', path_to_appdata, 'savedgames')
-        self.backup_file.add_files('Settings', path_to_appdata, 'user.ltx')
+        def backup(self):
+            super().backup()
+            path_to_appdata = os.path.join(SteamGamesPath, folder, '_appdata_')
+            self.backup_file.add_folder('Saves', path_to_appdata, 'savedgames')
+            self.backup_file.add_files('Settings', path_to_appdata, 'user.ltx')
 
-    def restore(self):
-        super().restore()
-        path_to_appdata = os.path.join(SteamGamesPath, 'stalker shadow of chernobyl ru', '_appdata_')
-        self.restore_file.restore_folder('Saves', path_to_appdata, 'savedgames')
-        self.restore_files('Settings', path_to_appdata, 'user.ltx')
+        def restore(self):
+            super().restore()
+            path_to_appdata = os.path.join(SteamGamesPath, folder, '_appdata_')
+            self.restore_file.restore_folder('Saves', path_to_appdata, 'savedgames')
+            self.restore_files('Settings', path_to_appdata, 'user.ltx')
 
-    def detect(self):
-        if os.path.isdir(os.path.join(SteamGamesPath, 'stalker shadow of chernobyl ru')):
-            return True
-        return False
+        def detect(self):
+            if os.path.isdir(os.path.join(SteamGamesPath, folder)):
+                return True
+            return False
+
+    return TempPlugin
+
+
+STALKERPlugin = factory("S.T.A.L.K.E.R.: Shadow of Chernobyl (RU)", 'stalker shadow of chernobyl ru')
+STALKERPlugin = factory("S.T.A.L.K.E.R.: Clear Sky", 'stalker clear sky')
+STALKERPlugin = factory("S.T.A.L.K.E.R.: Call of Pripyat", 'stalker call of pripyat')
