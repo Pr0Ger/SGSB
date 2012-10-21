dc7c23f52dbe7f88fa0f51334fb17b612f12c55e
Pr0Ger <git@pr0ger.org>
2012-10-21 14:57:31 +0400

    Zombie Driver plugin

---
+++ plugins/Zombie_Driver.py
@@ -0,0 +1,30 @@
+import os
+from lib.base_plugin import BasePlugin
+
+
+class ZombieDriverPlugin(BasePlugin):
+    Name = "ZombieDriver"
+    support_os = ["Windows"]
+
+    def backup(self, _):
+        _.add_files('Settings', os.path.join(os.environ['APPDATA'], 'ZombieDriver'), [
+            'controller.cfg',
+            'Ogre17.cfg',
+            'ZombieDriver.cfg',
+        ])
+        _.add_folder('Saves', os.path.join(os.environ['APPDATA'], 'ZombieDriver'), 'Save')
+
+    def restore(self, _):
+        _.restore_files('Saves', os.path.join(os.environ['APPDATA'], 'ZombieDriver'), [
+            'controller.cfg',
+            'Ogre17.cfg',
+            'ZombieDriver.cfg',
+        ])
+        _.restore_folder('Saves', os.path.join(os.environ['APPDATA'], 'ZombieDriver'), 'Save')
+
+
+    def detect(self):
+        if self.current_os == 'Windows':
+            if os.path.isdir(os.path.join(os.environ['APPDATA'], 'ZombieDriver')):
+                return True
+        return False
