1fc40c3ab7be79dec2f12be2fb775bd1cd0fc85d
Pr0Ger <git@pr0ger.org>
2012-12-06 18:22:15 +0400

    OS X support for Osmos plugin

--- plugins/Osmos.py
+++ plugins/Osmos.py
@@ -5,15 +5,23 @@ from lib.paths import MyDocumentsPath
 
 class OsmosPlugin(BasePlugin):
     Name = "Osmos"
-    support_os = ["Windows"]
+    support_os = ["Windows", "Darwin"]
+
+    def init(self):
+        if self.current_os == 'Windows':
+            self.path = MyDocumentsPath
+
+        if self.current_os == 'Darwin':
+            self.path = os.path.join('~', 'Library', 'Application Support')
+            self.path = os.path.expanduser(self.path)
 
     def backup(self, _):
-        _.add_folder('Profiles', MyDocumentsPath, 'Osmos')
+        _.add_folder('Profiles', self.path, 'Osmos')
 
     def restore(self, _):
-        _.restore_folder('Profiles', MyDocumentsPath, 'Osmos')
+        _.restore_folder('Profiles', self.path, 'Osmos')
 
     def detect(self):
-        if os.path.isdir(os.path.join(MyDocumentsPath, 'Osmos')):
+        if os.path.isdir(os.path.join(self.path, 'Osmos')):
             return True
         return False
