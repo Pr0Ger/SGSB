5820164f9b4b115fd9ecfa0bfa6526725aa5319b
Pr0Ger <git@pr0ger.org>
2012-12-09 14:21:05 +0400

    OS X support for VVVVVV plugin

--- plugins/VVVVVV.py
+++ plugins/VVVVVV.py
@@ -5,15 +5,18 @@ from lib.paths import MyDocumentsPath
 
 class VVVVVVPlugin(BasePlugin):
     Name = "VVVVVV"
-    support_os = ["Windows"]
+    support_os = ["Windows", "Darwin"]
+
+    def init(self):
+        self.path = os.path.join(MyDocumentsPath, 'VVVVVV')
 
     def backup(self, _):
-        _.add_folder('Saves', os.path.join(MyDocumentsPath, 'VVVVVV'), 'Saves')
+        _.add_folder('Saves', self.path, 'Saves')
 
     def restore(self, _):
-        _.restore_folder('Saves', os.path.join(MyDocumentsPath, 'VVVVVV'), 'Saves')
+        _.restore_folder('Saves', self.path, 'Saves')
 
     def detect(self):
-        if os.path.isdir(os.path.join(MyDocumentsPath, 'VVVVVV', 'Saves')):
+        if os.path.isdir(os.path.join(self.path, 'Saves')):
             return True
         return False
