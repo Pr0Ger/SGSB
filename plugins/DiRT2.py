ce39597708fc5936003c568717b5f28c768bddf2
Pr0Ger <git@pr0ger.org>
2013-01-15 21:24:30 +0400

    DiRT 2 plugin

---
+++ plugins/DiRT2.py
@@ -0,0 +1,19 @@
+import os
+from lib.base_plugin import BasePlugin
+from lib.paths import MyDocumentsPath
+
+
+class Dirt2Plugin(BasePlugin):
+    Name = "Colin McRae DiRT 2"
+    support_os = ["Windows"]
+
+    def backup(self, _):
+        _.add_folder('Saves', os.path.join(MyDocumentsPath, 'My Games'), 'DiRT2')
+
+    def restore(self, _):
+        _.restore_folder('Saves', os.path.join(MyDocumentsPath, 'My Games'), 'DiRT2')
+
+    def detect(self):
+        if os.path.isdir(os.path.join(MyDocumentsPath, 'My Games', 'DiRT2')):
+            return True
+        return False
