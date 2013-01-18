d540c74c4aaa8cd6acbf0f51026ff77cb779566b
Pr0Ger <git@pr0ger.org>
2013-01-18 15:45:00 +0400

    DiRT 3 plugin

---
+++ plugins/DiRT3.py
@@ -0,0 +1,20 @@
+import os
+from lib.base_plugin import BasePlugin
+from lib.paths import MyDocumentsPath
+
+
+class Dirt3Plugin(BasePlugin):
+    Name = "DiRT 3"
+    dependencies = ["Games for Windows Live profile"]
+    support_os = ["Windows"]
+
+    def backup(self, _):
+        _.add_folder('Saves', os.path.join(MyDocumentsPath, 'My Games'), 'DiRT3')
+
+    def restore(self, _):
+        _.restore_folder('Saves', os.path.join(MyDocumentsPath, 'My Games'), 'DiRT3')
+
+    def detect(self):
+        if os.path.isdir(os.path.join(MyDocumentsPath, 'My Games', 'DiRT3')):
+            return True
+        return False
