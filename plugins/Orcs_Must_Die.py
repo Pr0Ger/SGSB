d2b14bbf3beb9eb07ee45e4a6a6e4fab7519f772
Pr0Ger <git@pr0ger.org>
2013-10-05 22:06:21 +0400

    Added support for Orcs Must Die! 2

--- plugins/Orcs_Must_Die.py
+++ plugins/Orcs_Must_Die.py
@@ -3,17 +3,23 @@ from lib.base_plugin import BasePlugin
 from lib.paths import SteamCloudPath, SteamGamesPath
 
 
-class OrcsMustDiePlugin(BasePlugin):
-    Name = "Orcs Must Die!"
-    support_os = ["Windows"]
+def factory(name, id, folder):
+    class TempPlugin(BasePlugin):
+        Name = name
+        support_os = ["Windows"]
 
-    def backup(self, _):
-        _.add_folder('Data', os.path.join(SteamCloudPath, '102600'), 'remote')
+        def backup(self, _):
+            _.add_folder('Data', os.path.join(SteamCloudPath, id), 'remote')
 
-    def restore(self, _):
-        _.restore_folder('Data', os.path.join(SteamCloudPath, '102600'), 'remote')
+        def restore(self, _):
+            _.restore_folder('Data', os.path.join(SteamCloudPath, id), 'remote')
 
-    def detect(self):
-        if os.path.isdir(os.path.join(SteamGamesPath, 'orcs must die!')):
-            return True
-        return False
+        def detect(self):
+            if os.path.isdir(os.path.join(SteamGamesPath, folder)):
+                return True
+            return False
+
+    return TempPlugin
+
+OrcsMustDie = factory("Orcs Must Die!", '102600', 'orcs must die!')
+OrcsMustDie2 = factory("Orcs Must Die! 2", '201790', 'Orcs Must Die 2')
