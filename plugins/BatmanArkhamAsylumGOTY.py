92d8ec6b5bcd818fab93af71af6f9bdf157cc1cb
Pr0Ger <git@pr0ger.org>
2013-11-02 18:18:24 +0400

    Fixed Batman Arkham Asylum plugin to support recent update

--- plugins/BatmanArkhamAsylumGOTY.py
+++ plugins/BatmanArkhamAsylumGOTY.py
@@ -1,20 +1,19 @@
 import os
 from lib.base_plugin import BasePlugin
-from lib.paths import MyDocumentsPath
+from lib.paths import SteamCloudPath, SteamGamesPath
 
 
 class BatmanArkhamAsylumGOTYPlugin(BasePlugin):
     Name = "Batman: Arkham Asylum Game of the Year Edition"
-    dependencies = ["Games for Windows Live profile"]
     support_os = ["Windows"]
 
     def backup(self, _):
-        _.add_folder('Saves', os.path.join(MyDocumentsPath, 'Square Enix'), 'Batman Arkham Asylum GOTY')
+        _.add_folder('Saves', os.path.join(SteamCloudPath, '35140'), 'remote')
 
     def restore(self, _):
-        _.restore_folder('Saves', os.path.join(MyDocumentsPath, 'Square Enix'), 'Batman Arkham Asylum GOTY')
+        _.restore_folder('Saves', os.path.join(SteamCloudPath, '35140'), 'remote')
 
     def detect(self):
-        if os.path.isdir(os.path.join(MyDocumentsPath, 'Square Enix', 'Batman Arkham Asylum GOTY')):
+        if os.path.isdir(os.path.join(SteamGamesPath, 'Batman Arkham Asylum GOTY')):
             return True
         return False
