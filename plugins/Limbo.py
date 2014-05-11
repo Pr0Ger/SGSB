c7a30892a2f6bef59428728b444d9dcbfa906c5f
Pr0Ger <git@pr0ger.org>
2014-05-11 15:56:11 +0400

    LIMBO plugin

---
+++ plugins/Limbo.py
@@ -0,0 +1,19 @@
+import os
+from lib.base_plugin import BasePlugin
+from lib.paths import SteamCloudPath, SteamGamesPath
+
+
+class LIMBOPlugin(BasePlugin):
+    Name = "LIMBO"
+    support_os = ["Windows"]
+
+    def backup(self, _):
+        _.add_folder('Data', os.path.join(SteamCloudPath, '48000'), 'remote')
+
+    def restore(self, _):
+        _.restore_folder('Data', os.path.join(SteamCloudPath, '48000'), 'remote')
+
+    def detect(self):
+        if os.path.isdir(os.path.join(SteamGamesPath, 'limbo')):
+            return True
+        return False
