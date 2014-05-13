0afda2522c471ded59e743406dacd2c2163fbb3d
Pr0Ger <git@pr0ger.org>
2014-05-13 22:16:52 +0400

    Added OS X support for Prison Architect

--- plugins/Prison_Architect.py
+++ plugins/Prison_Architect.py
@@ -4,15 +4,23 @@ from lib.base_plugin import BasePlugin
 
 class PrisonArchitectPlugin(BasePlugin):
     Name = "Prison Architect"
-    support_os = ["Windows"]
+    support_os = ["Windows", "Darwin"]
+
+    def init(self):
+        if self.current_os == 'Windows':
+            self.path = os.path.join(os.environ['LOCALAPPDATA'], 'Introversion')
+
+        if self.current_os == 'Darwin':
+            self.path = os.path.join('~', 'Library', 'Application Support')
+            self.path = os.path.expanduser(self.path)
 
     def backup(self, _):
-        _.add_folder('Data', os.path.join(os.environ['LOCALAPPDATA'], 'Introversion'), 'Prison Architect')
+        _.add_folder('Data', self.path, 'Prison Architect')
 
     def restore(self, _):
-        _.restore_folder('Data', os.path.join(os.environ['LOCALAPPDATA'], 'Introversion'), 'Prison Architect')
+        _.restore_folder('Data', self.path, 'Prison Architect')
 
     def detect(self):
-        if os.path.isdir(os.path.join(os.environ['LOCALAPPDATA'], 'Introversion', 'Prison Architect')):
+        if os.path.isdir(os.path.join(self.path, 'Prison Architect')):
             return True
         return False
