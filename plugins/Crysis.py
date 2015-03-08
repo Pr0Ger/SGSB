ec2420394fbd79804e0e1d3894886d4ea26fc053
Sergey Petrov <git@pr0ger.org>
2015-03-08 11:29:19 +0300

    Fixed Crysis plugins

--- plugins/Crysis.py
+++ plugins/Crysis.py
@@ -1,6 +1,6 @@
 import os
 from lib.base_plugin import BasePlugin
-from lib.paths import MyDocumentsPath
+from lib.paths import MyDocumentsPath, SavedGamesPath
 
 
 def factory(name, folder):
@@ -9,22 +9,22 @@ def factory(name, folder):
         support_os = ["Windows"]
 
         def backup(self, _):
-            _.add_folder('Profiles', os.path.join(MyDocumentsPath, 'My Games', folder), 'Profiles')
-            _.add_folder('Saves', os.path.join(MyDocumentsPath, 'My Games', folder), 'SaveGames')
-            _.add_files('Config', os.path.join(MyDocumentsPath, 'My Games', folder), 'game.cfg')
+            _.add_folder('Profiles', folder, 'Profiles')
+            _.add_folder('Saves', folder, 'SaveGames')
+            _.add_files('Config', folder, 'game.cfg')
 
         def restore(self, _):
-            _.restore_folder('Profiles', os.path.join(MyDocumentsPath, 'My Games', folder), 'Profiles')
-            _.restore_folder('Saves', os.path.join(MyDocumentsPath, 'My Games', folder), 'SaveGames')
-            _.restore_files('Config', os.path.join(MyDocumentsPath, 'My Games', folder), 'game.cfg')
+            _.restore_folder('Profiles', folder, 'Profiles')
+            _.restore_folder('Saves', folder, 'SaveGames')
+            _.restore_files('Config', folder, 'game.cfg')
 
         def detect(self):
-            if os.path.isdir(os.path.join(MyDocumentsPath, 'My Games', folder)):
+            if os.path.isdir(folder):
                 return True
             return False
 
     return TempPlugin
 
-CrysisPlugin = factory("Crysis", 'Crysis')
-CrysisWarheadPlugin = factory("Crysis Warhead", 'Crysis_WARHEAD')
-Crysic2Plugin = factory("Crysis 2", "Crysis2")
+CrysisPlugin = factory("Crysis", os.path.join(MyDocumentsPath, 'My Games', 'Crysis'))
+CrysisWarheadPlugin = factory("Crysis Warhead", os.path.join(MyDocumentsPath, 'My Games', 'Crysis_WARHEAD'))
+Crysic2Plugin = factory("Crysis 2", os.path.join(SavedGamesPath, 'Crysis2'))
