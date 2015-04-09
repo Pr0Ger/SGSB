b6fca90329534b7acddff89a380e5578c9001b5f
Sergey Petrov <git@pr0ger.org>
2015-04-09 21:05:56 +0300

    Initial stub for HL2 series plugins

---
+++ plugins/HL2_series.py
@@ -0,0 +1,25 @@
+import os
+from lib.base_plugin import BasePlugin
+from lib.paths import SteamGamesPath
+
+
+def factory(name, folder):
+    class TempPlugin(BasePlugin):
+        Name = name
+        support_os = ["Windows"]
+
+        def backup(self, _):
+            _.add_folder('Saves', os.path.join(SteamGamesPath, folder, 'hl2'), 'save')
+
+        def restore(self, _):
+            _.restore_folder('Saves', os.path.join(SteamGamesPath, folder, 'hl2'), 'save')
+
+        def detect(self):
+            if os.path.isdir(os.path.join(SteamGamesPath, folder)):
+                return True
+            return False
+
+    return TempPlugin
+
+
+HL2UpdatePlugin = factory("Half-Life 2: Update", 'Half-Life 2 Update')
