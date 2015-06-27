c1826c22983668f7f8c3e8d925d2d980706dc725
Sergey Petrov <git@pr0ger.org>
2015-06-27 18:28:04 +0300

    Assassin's Creed Revelations plugin

---
+++ plugins/Assassins_Creed_Revelations.py
@@ -0,0 +1,21 @@
+import os
+from lib.base_plugin import BasePlugin
+from lib.paths import UbisoftSaves
+
+
+class AssassinsCreedRevelationsPlugin(BasePlugin):
+    Name = "Assassin's Creed Revelations"
+    support_os = ["Windows"]
+
+    def backup(self, _):
+        _.add_folder('Settings', MyDocumentsPath, 'Assassin\'s Creed Revelations')
+        _.add_folder('Saves', UbisoftSaves, '40')
+
+    def restore(self, _):
+        _.restore_folder('Settings', MyDocumentsPath, 'Assassin\'s Creed Revelations')
+        _.restore_folder('Saves', UbisoftSaves, '40')
+
+    def detect(self):
+        if os.path.isdir(MyDocumentsPath, 'Assassin\'s Creed Revelations')):
+            return True
+        return False
